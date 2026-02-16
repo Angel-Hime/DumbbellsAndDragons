46696049e931960b87d6f203c2b7222de02c07f5
Nene <nene.nnoruka@gmail.com>
2026-02-16 16:56:09 +0000

    bug: pool causing deploy failure

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -2,20 +2,18 @@
 
 import pg from "pg";
 
-const db = new pg.Pool({
+export const db = new pg.Pool({
   connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-pool.on("connect", () => {
+db.on("connect", () => {
   console.log("Database connected successfully");
 });
 
-pool.on("error", (err) => {
+db.on("error", (err) => {
   console.error("Unexpected database error:", err);
   process.exit(-1);
 });
-
-export default db;
